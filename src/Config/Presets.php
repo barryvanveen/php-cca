744006391fd0948a762ac76df97b34a49a718b23
= <barryvanveen@gmail.com>
2018-02-17 15:33:47 +0100

    order presets alphabetically

--- src/Config/Presets.php
+++ src/Config/Presets.php
@@ -7,23 +7,23 @@ use Barryvanveen\CCA\Exceptions\InvalidPresetException;
 class Presets
 {
     const PRESET_313 = '313';
-    const PRESET_GH = 'gh';
-    const PRESET_LAVALAMP = 'lavalamp';
     const PRESET_AMOEBA = 'amoeba';
     const PRESET_CCA = 'cca';
     const PRESET_CUBISM = 'cubism';
     const PRESET_CYCLIC_SPIRALS = 'cyclic_spirals';
+    const PRESET_GH = 'gh';
+    const PRESET_LAVALAMP = 'lavalamp';
     const PRESET_PERFECT = 'perfect';
     const PRESET_SQUARISH_SPIRALS = 'squarish_spirals';
 
     const VALID_PRESETS = [
         Presets::PRESET_313,
-        Presets::PRESET_GH,
-        Presets::PRESET_LAVALAMP,
         Presets::PRESET_AMOEBA,
         Presets::PRESET_CCA,
         Presets::PRESET_CUBISM,
         Presets::PRESET_CYCLIC_SPIRALS,
+        Presets::PRESET_GH,
+        Presets::PRESET_LAVALAMP,
         Presets::PRESET_PERFECT,
         Presets::PRESET_SQUARISH_SPIRALS,
     ];
@@ -34,18 +34,6 @@ class Presets
             Options::NEIGHBORHOOD_TYPE => NeighborhoodOptions::NEIGHBORHOOD_TYPE_MOORE,
             Options::STATES => 3,
             Options::THRESHOLD => 3,
-        ],
-        self::PRESET_GH => [
-            Options::NEIGHBORHOOD_SIZE => 3,
-            Options::NEIGHBORHOOD_TYPE => NeighborhoodOptions::NEIGHBORHOOD_TYPE_MOORE,
-            Options::STATES => 8,
-            Options::THRESHOLD => 5,
-        ],
-        self::PRESET_LAVALAMP => [
-            Options::NEIGHBORHOOD_SIZE => 2,
-            Options::NEIGHBORHOOD_TYPE => NeighborhoodOptions::NEIGHBORHOOD_TYPE_MOORE,
-            Options::STATES => 3,
-            Options::THRESHOLD => 10,
         ],
         self::PRESET_AMOEBA => [
             Options::NEIGHBORHOOD_SIZE => 3,
@@ -70,6 +58,18 @@ class Presets
             Options::NEIGHBORHOOD_TYPE => NeighborhoodOptions::NEIGHBORHOOD_TYPE_MOORE,
             Options::STATES => 8,
             Options::THRESHOLD => 5,
+        ],
+        self::PRESET_GH => [
+            Options::NEIGHBORHOOD_SIZE => 3,
+            Options::NEIGHBORHOOD_TYPE => NeighborhoodOptions::NEIGHBORHOOD_TYPE_MOORE,
+            Options::STATES => 8,
+            Options::THRESHOLD => 5,
+        ],
+        self::PRESET_LAVALAMP => [
+            Options::NEIGHBORHOOD_SIZE => 2,
+            Options::NEIGHBORHOOD_TYPE => NeighborhoodOptions::NEIGHBORHOOD_TYPE_MOORE,
+            Options::STATES => 3,
+            Options::THRESHOLD => 10,
         ],
         self::PRESET_PERFECT => [
             Options::NEIGHBORHOOD_SIZE => 1,
